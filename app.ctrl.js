dcad38cfae4d507078ad542e06b0bd72d481722c
Serene Ko <hazel.r.ko@gmail.com>
2026-02-27 00:37:15 -0500

    add mustache for home view

--- app.ctrl.js
+++ app.ctrl.js
@@ -1,12 +1,24 @@
 const express = require("express");
+const mustacheExpress = require("mustache-express");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// basic route
+// Mustache setup
+app.engine("mustache", mustacheExpress());
+app.set("view engine", "mustache");
+app.set("views", path.join(__dirname, "views"));
+
+
+// Route renders a view
 app.get("/", (req, res) => {
-  res.send("FloodDash server is running");
+  res.render("home", {
+    title: "FloodDash",
+    message: "Home page loaded."
+  });
 });
+
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
